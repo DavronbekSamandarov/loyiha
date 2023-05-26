822f3ca533acf0dfdc20a8b166afbb62f25f17bf
DavronbekSamandarov <134182773+DavronbekSamandarov@users.noreply.github.com>
2023-05-26 10:59:18 +0500

    qoshimcha
    fayl

--- script.js
+++ script.js
@@ -1,21 +1,92 @@
-// JavaScript qidirish funksiyasi
-document.addEventListener('DOMContentLoaded', function() {
-    var searchInput = document.getElementById('search-input');
+var addJobButton = document.getElementById('add-job-button');
+addJobButton.addEventListener('click', addNewJob);
+
+function addNewJob(event) {
+    event.preventDefault();
   
-    searchInput.addEventListener('input', function() {
-      var searchValue = searchInput.value.toLowerCase();
-      var jobListings = document.querySelectorAll('.job-listing');
+    var jobTitleInput = document.getElementById('job-title-input');
+    var companyNameInput = document.getElementById('company-name-input');
+    var jobDescriptionInput = document.getElementById('job-description-input');
   
-      for (var i = 0; i < jobListings.length; i++) {
-        var jobTitle = jobListings[i].getElementsByTagName('h3')[0].innerText.toLowerCase();
-        var jobDescription = jobListings[i].getElementsByTagName('p')[3].innerText.toLowerCase();
+    var jobTitle = jobTitleInput.value;
+    var companyName = companyNameInput.value;
+    var jobDescription = jobDescriptionInput.value;
   
-        if (jobTitle.includes(searchValue) || jobDescription.includes(searchValue)) {
-          jobListings[i].classList.add('highlighted');
-        } else {
-          jobListings[i].classList.remove('highlighted');
-        }
-      }
-    });
-  });
+    if (jobTitle === '' || companyName === '' || jobDescription === '') {
+      alert("Iltimos, barcha maydonlarni to'ldiring.");
+      return;
+    }
   
+    var newJob = {
+      title: jobTitle,
+      company: companyName,
+      description: jobDescription
+    };
+  
+    // Daha önce kaydedilmiş iş ilanlarını al
+    var savedJobs = JSON.parse(localStorage.getItem('jobs')) || [];
+    
+    // Yeni iş ilanını kaydet
+    savedJobs.push(newJob);
+    
+    // Güncellenmiş iş ilanlarını localStorage'e kaydet
+    localStorage.setItem('jobs', JSON.stringify(savedJobs));
+  
+    var jobListingsContainer = document.getElementById('job-listings');
+  
+    var jobListing = document.createElement('div');
+    jobListing.className = 'job-listing';
+  
+    var titleElement = document.createElement('h2');
+    titleElement.className = 'job-title';
+    titleElement.textContent = jobTitle;
+  
+    var companyElement = document.createElement('p');
+    companyElement.className = 'company-name';
+    companyElement.textContent = companyName;
+  
+    var descriptionElement = document.createElement('p');
+    descriptionElement.className = 'job-description';
+    descriptionElement.textContent = jobDescription;
+  
+    jobListing.appendChild(titleElement);
+    jobListing.appendChild(companyElement);
+    jobListing.appendChild(descriptionElement);
+  
+    jobListingsContainer.appendChild(jobListing);
+  
+    jobTitleInput.value = '';
+    companyNameInput.value = '';
+    jobDescriptionInput.value = '';
+  
+    alert("Yangi ma'lumot muvaffaqiyatli qo'shildi.");
+  }
+  
+function searchJobs(keyword) {
+  var jobListings = document.getElementsByClassName('job-listing');
+
+  for (var i = 0; i < jobListings.length; i++) {
+    var jobListing = jobListings[i];
+    var titleElement = jobListing.getElementsByClassName('job-title')[0];
+    var companyElement = jobListing.getElementsByClassName('company-name')[0];
+    var descriptionElement = jobListing.getElementsByClassName('job-description')[0];
+
+    var title = titleElement.textContent.toLowerCase();
+    var company = companyElement.textContent.toLowerCase();
+    var description = descriptionElement.textContent.toLowerCase();
+
+    if (title.includes(keyword.toLowerCase()) || company.includes(keyword.toLowerCase()) || description.includes(keyword.toLowerCase())) {
+      jobListing.style.display = 'block';
+    } else {
+      jobListing.style.display = 'none';
+    }
+  }
+}
+
+var searchButton = document.getElementById('search-button');
+searchButton.addEventListener('click', function() {
+  var searchInput = document.getElementById('search-input');
+  var keyword = searchInput.value;
+
+  searchJobs(keyword);
+});
